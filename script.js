58d4df9f2920c865444771ce34ad0c5ab2331117
Anand <aanand.raj.patna@gmail.com>
2022-10-08 20:04:01 +0530

    Hex color

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const hexColor = document.querySelector('.hex-color');
 const hexGenerate = document.querySelector('.hex-generate');
+const box = document.querySelector('.Generate-box');
 
 const colorgenerate = () => {
   const colornum = Math.random().toString(16).substring(2, 8);
   document.body.style.backgroundColor = '#' + colornum;
   hexColor.innerHTML = '#' + colornum;
+  box.style.backgroundColor = `#${colornum}`;
 };
 
 //Getting random color on clicking button
 hexGenerate.addEventListener('click', colorgenerate);
 
-//To get random color on every loading of page
+//To get random color whenever page load
 colorgenerate();
